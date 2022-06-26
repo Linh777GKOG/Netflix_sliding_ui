4be9c81201a026524d65d03ac36d6ca1f3bf3c77
Linh Vu <linh777gkog@gmail.com>
2022-06-26 22:30:53 +0700

    lang nghe su kien click

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('click', (e) => {
+  let handle;
+  if (e.target.matches('.handle')) {
+    handle = e.target;
+  } else {
+    handle = e.target.closest('.handle');
+  }
+  if (handle != null) onHandleClick(handle);
+});
