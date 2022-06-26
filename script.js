40344bd0f325a5b7db0bff5c40d776e80d868691
Linh Vu <linh777gkog@gmail.com>
2022-06-26 22:31:43 +0700

    tao ham onhandclick

--- script.js
+++ script.js
@@ -41,3 +41,35 @@ function calculateProgressBar(progressBar) {
     progressBar.append(barItem);
   }
 }
+
+function onHandleClick(handle) {
+  const progressBar = handle.closest('.row').querySelector('.progress-bar');
+  const slider = handle.closest('.container').querySelector('.slider');
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue('--slider-index')
+  );
+  const progressBarItemCount = progressBar.children.length;
+  if (handle.classList.contains('left-handle')) {
+    if (sliderIndex - 1 < 0) {
+      slider.style.setProperty('--slider-index', progressBarItemCount - 1);
+      progressBar.children[sliderIndex].classList.remove('active');
+      progressBar.children[progressBarItemCount - 1].classList.add('active');
+    } else {
+      slider.style.setProperty('--slider-index', sliderIndex - 1);
+      progressBar.children[sliderIndex].classList.remove('active');
+      progressBar.children[sliderIndex - 1].classList.add('active');
+    }
+  }
+
+  if (handle.classList.contains('right-handle')) {
+    if (sliderIndex + 1 >= progressBarItemCount) {
+      slider.style.setProperty('--slider-index', 0);
+      progressBar.children[sliderIndex].classList.remove('active');
+      progressBar.children[0].classList.add('active');
+    } else {
+      slider.style.setProperty('--slider-index', sliderIndex + 1);
+      progressBar.children[sliderIndex].classList.remove('active');
+      progressBar.children[sliderIndex + 1].classList.add('active');
+    }
+  }
+}
